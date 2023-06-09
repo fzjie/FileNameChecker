95f6fd854885b410be0b8e4e86202bd6cc57c6aa
fzjie <fzjie1006@163.com>
2023-06-09 21:37:35 +0800

    v1.0

---
+++ MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace FileNameChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.SubToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(582, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(21, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件夹路径";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(703, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(784, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "分析";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(24, 53);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(835, 365);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "文件路径";
+            this.columnHeader1.Width = 763;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "长度";
+            this.columnHeader2.Width = 68;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(883, 25);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem1
+            // 
+            this.ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SubToolStripMenuItem1});
+            this.ToolStripMenuItem1.Name = "ToolStripMenuItem1";
+            this.ToolStripMenuItem1.Size = new System.Drawing.Size(44, 21);
+            this.ToolStripMenuItem1.Text = "选项";
+            // 
+            // SubToolStripMenuItem1
+            // 
+            this.SubToolStripMenuItem1.Name = "SubToolStripMenuItem1";
+            this.SubToolStripMenuItem1.Size = new System.Drawing.Size(100, 22);
+            this.SubToolStripMenuItem1.Text = "设置";
+            this.SubToolStripMenuItem1.Click += new System.EventHandler(this.SubToolStripMenuItem1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 459);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(883, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(766, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "就绪";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(883, 434);
+            this.panel1.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(883, 481);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainForm";
+            this.Text = "FileNameChecker";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem SubToolStripMenuItem1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileNameChecker
+{
+    public partial class MainForm : Form
+    {
+        private string scanPath;
+        private int maxLength = 113;
+        double formWidth;
+        double formHeight;
+        double scaleX;
+        double scaleY;
+        Dictionary<string, string> controlsInfo = new Dictionary<string, string>();
+        public MainForm()
+        {
+            InitializeComponent();
+            GetAllInitInfo(this.Controls[0]);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.scanPath))
+            {
+                MessageBox.Show("请选择路径");
+            }else {
+                listView1.BeginUpdate();
+                string[] files = Directory.GetFiles(this.scanPath, "*.*",SearchOption.AllDirectories);
+                toolStripProgressBar1.Maximum = files.Length;
+                int longFileName=0;
+                toolStripProgressBar1.Value = 0;
+                foreach (string file in files) {
+                    string fileName=Path.GetFileName(file);
+                    if (fileName.Length > this.maxLength) {
+                        ListViewItem listViewItem = new ListViewItem(file);
+                        listViewItem.SubItems.Add(fileName.Length.ToString());
+                        listView1.Items.Add(listViewItem);
+                        longFileName++;
+                    }
+                    toolStripProgressBar1.Value++;
+                }
+                listView1.EndUpdate();
+                toolStripStatusLabel1.Text = "分析结果：" + "扫描到文件"+files.Length.ToString()+"个，文件名过长文件" + longFileName.ToString() + "个。";
+            }
+        }
+
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("Explorer.exe");
+            psi.Arguments = "/e,/select,"+ listView1.SelectedItems[0].Text;
+            Process.Start(psi);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.scanPath = textBox1.Text;
+        }
+
+
+        private void SubToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Settings settingsForm = new Settings(this.maxLength);
+            settingsForm.ShowDialog();
+            if (settingsForm.DialogResult == DialogResult.OK) {
+                this.maxLength = settingsForm.maxLength;
+            }
+            settingsForm.Dispose();
+        }
+
+        private void GetAllInitInfo(Control ctrlContainer)
+        {
+            if (ctrlContainer.Parent == this)
+            {
+                this.formWidth = Convert.ToDouble(ctrlContainer.Width);
+                this.formHeight = Convert.ToDouble(ctrlContainer.Height);
+            }
+            foreach (Control item in ctrlContainer.Controls)
+            {
+                if (item.Name.Trim() != "")
+                {
+                    this.controlsInfo.Add(item.Name, (item.Left + item.Width / 2) + "," + (item.Top + item.Height / 2) + "," + item.Width + "," + item.Height + "," + item.Font.Size);
+                }
+                if ((item as UserControl) == null && item.Controls.Count > 0)
+                {
+                    GetAllInitInfo(item);
+                }
+            }
+
+        }
+
+        private void ControlsChangeInit(Control ctrlContainer)
+        {
+            this.scaleX = (Convert.ToDouble(ctrlContainer.Width) / this.formWidth);
+            this.scaleY = (Convert.ToDouble(ctrlContainer.Height) / this.formHeight);
+        }
+
+        private void ControlsChange(Control ctrlContainer)
+        {
+            double[] pos = new double[5];
+            foreach (Control item in ctrlContainer.Controls)
+            {
+                if (item.Name.Trim() != "")
+                {
+                    if ((item as UserControl) == null && item.Controls.Count > 0)
+                    {
+                        ControlsChange(item);
+                    }
+                    string[] strs = this.controlsInfo[item.Name].Split(',');
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        pos[i] = Convert.ToDouble(strs[i]);
+                    }
+                    double itemWidth = pos[2] * this.scaleX;
+                    double itemHeight = pos[3] * this.scaleY;
+                    item.Left = Convert.ToInt32(pos[0] * this.scaleX - itemWidth / 2);
+                    item.Top = Convert.ToInt32(pos[1] * this.scaleY - itemHeight / 2);
+                    item.Width = Convert.ToInt32(itemWidth);
+                    item.Height = Convert.ToInt32(itemHeight);
+                    item.Font = new Font(item.Font.Name, float.Parse((pos[4] * Math.Min(this.scaleX, this.scaleY)).ToString()));//字体
+
+                }
+            }
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.controlsInfo.Count > 0)
+            {
+                ControlsChangeInit(this.Controls[0]);
+                ControlsChange(this.Controls[0]);
+            }
+        }
+    }
+}
